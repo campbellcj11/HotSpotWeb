01a907d3bb3be9e925a286fc7002902f52c9db47
lamurray <loganmurray1994@gmail.com>
2016-11-02 20:30:04 -0400

    Added db bulk upload and storage for firebase
    I left it commented so that way you can integrate it later

--- actions/firebaseInteraction.js
+++ actions/firebaseInteraction.js
@@ -23,3 +23,79 @@ query.on("value", function(snapshot) {
 }, function (error) {
 	console.log("Read error:" + error.code);
 });
+
+
+
+/*Storage*/
+
+/*
+//upload photo to firebase storage
+// Get elements
+var uploader = document.getElementById('uploader');
+var fileButton = document.getElementById('fileButton');
+
+//listen for file selection
+fileButton.addEventListener('change', function(e){
+	//get file
+	var file = e.target.files[0];
+
+	//create a storage ref
+	var storageRef = firebase.storage().ref('folder_name/' + file.name);
+
+	//upload file
+	var task = storageRef.put(file);
+
+	//update progrss bar
+	task.on('state_changed',
+
+		function progrss(snapshot){
+			var percentage = (snapshot.bytesTransferred / snapshot.totalbytes) * 100;
+			uploader.value = percentage;
+		},
+
+		function error(err){
+
+		}
+		function complete(){
+
+		}
+
+  );
+
+});
+
+
+//Download photo
+var storageRef = firebase.storage.ref("folderName/file.jpg");
+storageRef.getDownloadURL().then(function(url) {
+  console.log(url);
+});
+
+
+
+
+/*db*/
+/*
+var bulkUploadButton = document.getElementById('bulkUploadButton');
+
+bulkUploadButton.addEventListener('change', function(e){
+
+	var JSONItems = [];
+	d3.csv( e, function( data){
+	  JSONItems = data;
+	});
+
+	for(i = 0; i < JSONItems.length; i++)
+	{
+		var event = JSONItems[i];
+		export function pushEvent(event) {
+		  database.ref('events/').push(event));
+		  return {
+		    type: LOG_IN,
+		    currentUser: firebase.auth().currentUser
+		  }
+		}
+
+	}
+}
+*/
