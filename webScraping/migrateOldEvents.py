17cb6fe06c89fe3ca33f8ec6f939c5ce9cafaad4
Conor Campbell <campbellcj11@gmail.com>
2017-03-22 15:19:53 -0400

    First initial version of achiver. Still needs work before running. Do not run it yet.

---
+++ webScraping/migrateOldEvents.py
@@ -0,0 +1,52 @@
+import pyrebase
+import sys
+import getpass
+import time
+
+# config = {
+#     "apiKey" : "AIzaSyBc6_49WEUZLKCBoR8FFIHAfVjrZasdHlc",
+#     "authDomain" : "projectnow-964ba.firebaseapp.com",
+#     "databaseURL" : "https://projectnow-964ba.firebaseio.com",
+#     "storageBucket" : "projectnow-964ba.appspot.com",
+# }
+## test database
+config = {
+    "apiKey": "AIzaSyBtEU6cCFmGaUSrteSyrg8SDgiOsCaAJOo",
+    "authDomain": "projectnowtest.firebaseapp.com",
+    "databaseURL": "https://projectnowtest.firebaseio.com",
+    "storageBucket": "projectnowtest.appspot.com",
+    "messagingSenderId": "727912823537"
+};
+
+firebase = pyrebase.initialize_app(config)
+
+def setup():
+    print("Setting up archiver...")
+    auth = firebase.auth()
+    # Log the user in
+    # userName = input("Plese enter a user name for firebase: ")
+    userName = 'conor@email.sc.edu'
+    # password = input("Please enter a password for the previous account: ")
+    # user = auth.sign_in_with_email_and_password(userName, getpass.getpass())
+    user = auth.sign_in_with_email_and_password(userName, 'password')
+    return user;
+
+def migrateEvents(user, db):
+    print("Starting Achiver...")
+    databaseEvents = db.child("events").get()
+    timeNow = int(time.time()) * 1000 # for milliseconds
+    if databaseEvents.each() is None:
+        print("Error, database empty.")
+    else:
+        for locale in databaseEvents.each():
+            for key, data in locale.val().items():
+                if 'Date' in data:
+                    if (int(data['Date']) < timeNow):
+                        db.child("archivedEvents").child(locale.key()).update({key : data})
+                        db.child("events").child(locale.key()).child(key).remove()
+                        db.child("tags").child(key).remove()
+
+db = firebase.database()
+user = setup()
+migrateEvents(user, db)
+print("Achrive complete.")
