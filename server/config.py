e1d1a0980d2caa74281ae562c1aa50ac525000e3
Conor Campbell <campbellcj11@gmail.com>
2017-06-22 21:37:31 -0400

    Testing direct database connection.

--- server/config.py
+++ server/config.py
@@ -3,10 +3,12 @@ import os
 class Config(object):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://HotSpotAdmin:UscGrad2017@hotspotdb.cv91mewjlcfw.us-east-1.rds.amazonaws.com:5432/HotSpot'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://HotSpotAdmin:UscGrad2017@hotspotdb.cv91mewjlcfw.us-east-1.rds.amazonaws.com:5432/HotSpot'
     DEBUG = False
     TESTING = False
 
