9961c474a5684a2bc19fe7c578d1de809b04622f
Conor Campbell <campbellcj11@gmail.com>
2017-07-06 14:37:14 -0400

    Added first model of eventbrite scraper.

---
+++ webScraping/eventBriteScraping.py
@@ -0,0 +1,278 @@
+from eventbrite import Eventbrite
+from bs4 import BeautifulSoup
+import sys, psycopg2
+eventbrite = Eventbrite('3DFUMKJHM7JBPUDAWUJY')
+
+""" Connect to database using credentials."""
+def connectToDatabase():
+    print("Connecting to database...")
+    try:
+        conn = psycopg2.connect(dbname="HotSpot", user="HotSpotAdmin", password="UscGrad2017", host="hotspotdb.cv91mewjlcfw.us-east-1.rds.amazonaws.com", port="5432")
+        cur = conn.cursor()
+    except:
+        print('Could not connect to database.')
+        sys.exit()
+    return cur, conn
+
+def getCurrentLocales():
+    print("Getting Current Locales...")
+    localeSql = "SELECT * FROM locales;"
+    cur.execute(localeSql)
+    conn.commit()
+    locales = cur.fetchall()
+    dictOfLocales = {}
+    # Format into a city state string split by ' ::: '
+    for locale in locales:
+        dictOfLocales[locale[1] + " ::: " + locale[2]] = locale[0]
+
+    # print("Got Current Locales")
+    return dictOfLocales
+
+def refreshLocales(data):
+    # print("RefreshingLocales")
+    localeInsertSql = "INSERT INTO locales (name, state, country) VALUES (%s, %s, %s);"
+    localeData = (data['city'], data['state'], 'United States')
+    cur.execute(localeInsertSql, localeData)
+    localeSql = "SELECT * FROM locales;"
+    cur.execute(localeSql)
+    conn.commit()
+    locales = cur.fetchall()
+    dictOfLocales = {}
+    for locale in locales:
+        dictOfLocales[locale[1] + " ::: " + locale[2]] = locale[0]
+
+    # print("Got Current Locales")
+    return dictOfLocales
+
+def getEvents(this_latitude, this_longitude):
+    totalEvents = []
+    # get events within 10 miles
+    events = eventbrite.event_search(**{
+        'location.latitude': this_latitude,
+        'location.longitude': this_longitude,
+        'location.within' : '10mi'
+    })
+    if 'error' in events:
+        if events['error'] == 'HIT_RATE_LIMIT':
+            print("HIT RATE LIMIT.")
+            sys.exit()
+        else:
+            print(events['error'])
+            sys.exit()
+
+    for x in events['events']:
+        totalEvents.append(x)
+
+    totalPages = events['pagination']['page_count']
+    startPage = 2
+    while startPage <= totalPages:
+        events = eventbrite.event_search(**{
+            'location.latitude': this_latitude,
+            'location.longitude': this_longitude,
+            'location.within' : '10mi',
+            'page' : startPage
+        })
+        for x in events['events']:
+            # print(x)
+            totalEvents.append(x)
+        startPage += 1
+
+    return totalEvents
+
+def getVenueInfo(venue_id):
+    try:
+        venue = eventbrite.get('/venues/' + str(venue_id))
+        address_line1 = venue['address']['address_1'].title()
+        city = venue['address']['city']
+        zipCode = venue['address']['postal_code']
+        state = venue['address']['region']
+        address = address_line1 + ', ' + city + ', ' + state + ' ' + zipCode
+        returnVenue = {
+            'name' : venue['name'],
+            'address' : address,
+            'city' : city,
+            'state' : state
+        }
+        return returnVenue
+    except:
+        return None
+
+def parseDescription(description):
+    soup = BeautifulSoup(description, "html.parser")
+    paras = soup.find_all('p')
+    fullDescription = ''
+    for para in paras:
+        paraText = para.getText()
+        if len(paraText) > 3:
+            if paraText[-1:] == '.' or paraText[-1:] == '!':
+                fullDescription += (para.getText() + ' ')
+            else:
+                fullDescription += (para.getText() + '. ')
+    return fullDescription
+
+def constructTagMapping():
+    tagMapping = dict()
+    tagMapping['art'] = 'art'
+    tagMapping['arts'] = 'art'
+    tagMapping['art and film'] = 'art'
+    tagMapping['book'] = 'books'
+    tagMapping['books and literature'] = 'books'
+    tagMapping['causes'] = 'causes'
+    tagMapping['fundraiser'] = 'causes'
+    tagMapping['class'] = 'class'
+    tagMapping['lecture'] = 'class'
+    tagMapping['comedy'] = 'comedy'
+    tagMapping['community'] = 'community'
+    tagMapping['neighborhood'] = 'community'
+    tagMapping['festival'] = 'community'
+    tagMapping['home and garden'] = 'community'
+    tagMapping['family & education'] = 'community'
+    tagMapping['conference'] = 'conference'
+    tagMapping['dance'] = 'dance'
+    tagMapping['dining'] = 'food'
+    tagMapping['food and drink'] = 'food'
+    tagMapping['food & drink'] = 'food'
+    tagMapping['food and tasting'] = 'food'
+    tagMapping['fitness'] = 'health'
+    tagMapping['health'] = 'health'
+    tagMapping['health and wellness'] = 'health'
+    tagMapping['meetup'] = 'social'
+    tagMapping['networking'] = 'social'
+    tagMapping['family'] = 'social'
+    tagMapping['event and music'] = 'social'
+    tagMapping['event and party'] = 'social'
+    tagMapping['event and recreation'] = 'social'
+    tagMapping['event and art'] = 'social'
+    tagMapping['event and film'] = 'social'
+    tagMapping['event and cause'] = 'social'
+    tagMapping['event and food'] = 'social'
+    tagMapping['event and dance'] = 'social'
+    tagMapping['event'] = 'social'
+    tagMapping['games'] = 'sport'
+    tagMapping['sports & fitness'] = 'sport'
+    tagMapping['sports'] = 'sport'
+    tagMapping['sports and recreation'] = 'sport'
+    tagMapping['movie'] = 'movie'
+    tagMapping['music'] = 'music'
+    tagMapping['nightlife'] = 'nightlife'
+    tagMapping['parties and nightlife'] = 'nightlife'
+    tagMapping['theater'] = 'theater'
+    tagMapping['theater and dance'] = 'theater'
+    tagMapping['religion'] = 'religion'
+    tagMapping['religious'] = 'religion'
+    tagMapping['shopping'] = 'shopping'
+    tagMapping['other'] = 'other'
+    tagMapping['film & media'] = 'film'
+    tagMapping['charity & causes'] = 'community'
+    tagMapping['business'] = 'conference'
+    tagMapping['spirituality'] = 'religion'
+    tagMapping['science & tech'] = 'tech'
+    tagMapping['travel & outdoor'] = 'sport'
+    tagMapping['fashion'] = 'fashion'
+    return tagMapping
+
+"""Parse the short description from the long description."""
+def getShortDescription(longDescription):
+    maxCharacterLength = 200
+    indexOfPeriod = -1
+
+    for index, match in enumerate(longDescription[:maxCharacterLength]):
+        if ((indexOfPeriod < index) and (match == '.' or match == '!' or match == '?')):
+            indexOfPeriod = index
+
+    if (indexOfPeriod == -1):
+        shortDescription = longDescription[:maxCharacterLength - 1]
+    else:
+        shortDescription = longDescription[:indexOfPeriod + 1]
+    return shortDescription
+
+def getCurrentDatabaseEvents():
+    # check if event exists
+    existingEventsSql = "SELECT website FROM events WHERE website LIKE \'%eventbrite.com%\';"
+    cur.execute(existingEventsSql)
+    conn.commit()
+    existingEventsResult = [] if cur.description is None else cur.fetchall()
+    returnEvents = {}
+    for element in existingEventsResult:
+        returnEvents[element[0]] = True
+    return returnEvents
+
+
+def formatEvents(totalEvents, tagMapping):
+    dictOfLocales = getCurrentLocales()
+    currentDatabaseEvents = getCurrentDatabaseEvents()
+    throwAwayCounter = 0
+    addedCounter = 0
+    duplicateEventsCounter = 0
+    for event in totalEvents:
+        # locale_id, address, start_date, end_date, name, type, venue_name
+        # short_description, long_description, status, website
+        # image, price
+        try:
+            venue_id = event['venue_id']
+            venue = getVenueInfo(venue_id)
+            start_date = event['start']['utc']
+            end_date = event['end']['utc']
+            name = event['name']['html']
+            event_type = 'Event'
+            long_description = parseDescription(event['description']['html'])
+            short_description = getShortDescription(long_description)
+            # PENDING for now
+            status = 'pending'
+            website = event['url']
+            #image
+            image = event['logo']['url']
+            #category
+            category_id = event['category_id']
+            category = eventbrite.get('/categories/' + str(category_id))
+            tag = category['short_name'].lower()
+            if tag not in tagMapping:
+                print("Tag nonexistant, please add " + tag)
+                continue
+            else:
+                tag = tagMapping[tag]
+            # localeID
+            if venue['state'] == 'New Jersey':
+                cityStateString = venue['city'] + ' ::: NJ'
+            else:
+                cityStateString = venue['city'] + ' ::: ' + venue['state']
+        except:
+            throwAwayCounter += 1
+            continue
+
+        if cityStateString not in dictOfLocales:
+            print('Locale does not exist: ' + venue['city'])
+            # dictOfLocales = refreshLocales(venue)
+            # localeID = dictOfLocales[cityStateString]
+            throwAwayCounter += 1
+            pass
+        else:
+            localeID = dictOfLocales[cityStateString]
+            if website not in currentDatabaseEvents:
+                print("Adding.")
+                sql = "INSERT INTO events (locale_id, address, start_date, end_date, name, venue_name, type, short_description, long_description, status, website, image) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id;"
+                sqlData = (localeID, venue['address'], start_date, end_date, name, venue['name'], 'Event', short_description, long_description, status, website, image)
+                cur.execute(sql, sqlData)
+                id_of_new_row = int(cur.fetchone()[0])
+                tagSql = "INSERT INTO tags (name, event_id) VALUES (%s, %s);"
+                tagData = (tag, id_of_new_row)
+                cur.execute(tagSql, tagData)
+                conn.commit()
+                addedCounter += 1
+            else:
+                duplicateEventsCounter += 1
+                print('Not adding.')
+
+    return (addedCounter, throwAwayCounter, duplicateEventsCounter)
+
+
+
+cur, conn = connectToDatabase()
+tagMapping = constructTagMapping()
+current_latitude = 39.364283
+current_longitude = -74.422927
+totalEvents = getEvents(current_latitude, current_longitude)
+addedCounter, throwAwayCounter, duplicateEventsCounter = formatEvents(totalEvents, tagMapping)
+print("Added counter: " + str(addedCounter))
+print("Throw away counter: " + str(throwAwayCounter))
+print("Duplicate counter: " + str(duplicateEventsCounter))
